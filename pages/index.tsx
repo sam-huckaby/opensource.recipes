cdd406d22bc6c3b514700fceab1e298190d4e61b
Sam Huckaby <samhuckaby@gmail.com>
2022-10-24 22:24:34 -0400

    ideas to work on

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
+  const recipes = [
+    {
+      title: "A safer git rebase",
+      description: "A little more work, but a lot more piece of mind",
+    },
+    {
+      title: "Quick NextJs playground",
+      description: "Setting up a Nextjs playground in Vercel from the ground up",
+    },
+  ];
+
   return (
     <div className="flex min-h-screen flex-col py-8">
       <Head>
@@ -20,53 +31,16 @@ const Home: NextPage = () => {
           <span className="italic font-cursive-lettering -mt-8 text-8xl text-orange-600 self-end">Recipes</span>
         </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="mt-6 flex w-full flex-col items-center">
+          {
+            recipes.map(
+              ({title, description}) => <div className="border rounded-xl w-11/12 mt-16 relative p-6">
+                <a href="" className="border-t border-r border-l rounded-t-xl absolute -top-2 right-4">Check it out</a>
+                <span className="text-4xl">{title}</span>
+                <p>{description}</p>
+              </div>
+            )
+          }
         </div>
       </main>
 
